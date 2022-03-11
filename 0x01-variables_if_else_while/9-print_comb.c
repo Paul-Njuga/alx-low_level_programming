0d1390ae6cfc0aa735300303df308a8bdd6c5f57
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-11 14:18:47 -0800

    Commit 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations
+ * of single-digit numbers
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
