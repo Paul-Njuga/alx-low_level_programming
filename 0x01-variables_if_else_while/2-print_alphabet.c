8b62dec3f0535473ae858025e4b27c28ea6405cf
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-11 12:26:25 -0800

    Commit 2-print_alphabetic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Loop through alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for(i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(tolower(i));
+	}
+	putchar('\n');
+	return (0);
+}
