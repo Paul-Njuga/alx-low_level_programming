0305c5392cbd0bb47304c06f3fe294622f3869d7
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-11 12:59:27 -0800

    Commit 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Loop through alphabet
+ * except for 'e' & 'q'
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
