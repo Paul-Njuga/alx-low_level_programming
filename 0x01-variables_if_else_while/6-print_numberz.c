9d9689b499cbf5024f05b668294a4ece44499095
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-11 13:46:16 -0800

    Commit 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print 1 to 10
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
