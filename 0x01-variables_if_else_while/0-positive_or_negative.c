20f3ee48f2e55c1f5bc711b3ecc7d875659a618b
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-11 11:21:03 -0800

    Commit 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,12 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0) {
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
 	}
-	else {
+	else
+	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
