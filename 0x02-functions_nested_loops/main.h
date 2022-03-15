f9c2496ecc2f63ec5c63326d64952c5eae3ba0c1
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-15 16:11:14 -0700

    Commit Print_sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,11 +4,14 @@ int _putchar(char c);
 /* print_alphabet prototype */
 void print_alphabet(void);
 
-/*print_alphabet_x10 prototype */
+/* print_alphabet_x10 prototype */
 void print_alphabet_x10(void);
 
-/*_islower prototype */
+/* _islower prototype */
 int _islower(int c);
 
-/*_isalpha prototype */
+/* _isalpha prototype */
 int _isalpha(int c);
+
+/* print_sign prototype */
+int print_sign(int n);
