b2afafe4f53a69e2a6540cef62424de9ec14c703
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-15 15:11:44 -0700

    Commit Print alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Loop through alphabet
+ * print_alphabet_x10() - Loop through alphabet
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
