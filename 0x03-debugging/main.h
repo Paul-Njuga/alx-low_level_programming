e5ff24dab48b9b1b5f5210f83e571d6b1f4d4812
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-16 16:33:14 -0700

    Commit largest_number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,5 @@
-/*positive_or_negative prototype*/
+/* positive_or_negative prototype */
 void positive_or_negative(int i);
+
+/* largest_number prototype */
+int largest_number(int a, int b, int c)
