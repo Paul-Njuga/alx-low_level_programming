917fc1c62e17850c10e811987cb71af9147a85a0
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-17 14:40:06 -0700

    Commit more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j <= 10; j++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar('0' + i);
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchr('\n');
 	}
