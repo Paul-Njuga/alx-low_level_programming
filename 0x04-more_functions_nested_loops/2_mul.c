2406dda586e913491cacd689addd62af972d5deb
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-17 11:22:04 -0700

    Commit mul

---
+++ 0x04-more_functions_nested_loops/2_mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a - int to check
+ * @b - int to check
+ * Return: c
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = (a * b);
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,8 @@
 /* _isupper prototype */
 int _isupper(int c);
 
-/* _isdigit */
+/* _isdigit prototype */
 int _isdigit(int c);
+
+/* mu prototype */
+int mul(int a, int b);
