993f59919edcc78211d1ca577d86160488aea19b
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-17 14:07:29 -0700

    Commit print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_most_numbers - prints 0 to 9 using _putchar,
  * except 2 and 4, followed by a new line
- * Return: Always 0 (Success)
  */
 void print_most_numbers(void)
 {
@@ -11,8 +10,8 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 || i != 4)
-			_putchar(i + '0');
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
 	}
 	_putchar('\n');
 }
