e413e01696c12e1f64006da1d4e827b158d9037c
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-17 15:24:23 -0700

    Commit print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,17 @@
 void print_line(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
-	else
+	} else
 	{
 		int i;
 
-		for (i = 1; i < n; i++)
+		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
 	}
+
 }
