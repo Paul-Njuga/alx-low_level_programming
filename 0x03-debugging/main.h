28f6fd7c1604a747af00490bae489ee12682f905
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-16 17:11:43 -0700

    Commit remaining_days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,3 +3,9 @@ void positive_or_negative(int i);
 
 /* largest_number prototype */
 int largest_number(int a, int b, int c);
+
+/* convert_day */
+int convert_day(int month, int day);
+
+/* void print_remaining_days protoype */
+void print_remaining_days(int month, int day, int year);
