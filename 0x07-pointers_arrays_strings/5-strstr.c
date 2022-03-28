105dea93ab03f951a1b8328db605779986715bc4
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-03-28 14:36:51 -0700

    Commit task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,30 @@
 #include "main.h"
-
 /**
- * *_strstr - locates a substring
- * @haystack: string to search in
- * @needle: substring to look for
- *
- * Return: pointer to the beginning of the located substring
- * or NULL if the substring is not found
- */
+* *_strstr - locates a substring
+*@haystack: string to be scanned
+*@needle: string to be searced
+*Return: pointer of the first occurrence
+*/
 char *_strstr(char *haystack, char *needle)
 {
-char *bhaystack;
-char *pneedle;
+	int i, j, b;
 
-while (*haystack != '\0')
-{
-bhaystack = haystack;
-pneedle = needle;
-
-while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
-{
-haystack++;
-pneedle++;
-}
-if (!*pneedle)
-return (bhaystack);
-haystack = bhaystack + 1;
-}
-return (0);
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		b = i;
+		while (haystack[i] == needle[j] && haystack[i] != '\0'
+			&& needle[j] != '\0')
+		{
+			i++;
+			j++;
+		}
+		if (needle[j] == '\0')
+			return (haystack + b);
+		i = b + 1;
+	}
+	return ('\0');
 }
 ~
 ~
