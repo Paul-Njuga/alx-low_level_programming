52fc7a63cd2ea7702ac72e23aa403b8aaebe3d1e
Paul-Njuga <84309434+Paul-Njuga@users.noreply.github.com>
2022-04-26 06:29:48 -0700

    Commit task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
 * print_listint - function that prints all the elements of a listint_t list.
-*@h: header of the list
-*Return: the number of nodes
+* @h: header of the list
+* Return: the number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
