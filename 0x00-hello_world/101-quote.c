1179f8894a1b38362ab6b341288df07ebd2504c6
Paul Njuga <paulnjuga@gmail.com>
2022-10-14 17:23:04 -0700

    Create 101-qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints to stderr
+ *
+ * Return: 1 (Unsuccessful)
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
