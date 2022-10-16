9c66fef492f582941ebae728fd920f765617e868
Paul Njuga <paulnjuga@gmail.com>
2022-10-16 16:29:55 -0700

    Create 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int ch = 97;
+
+        for (ch = 97; ch <= 122; ch++)
+                putchar(ch);
+	for (ch = 65; ch <= 90; ch++)
+                putchar(ch);
+        putchar('\n');
+        return (0);
+}
