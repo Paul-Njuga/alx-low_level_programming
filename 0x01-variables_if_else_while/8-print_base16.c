f618a0c21a085c3e9d3dc9e608b690168a3f8447
Paul Njuga <paulnjuga@gmail.com>
2022-10-16 17:24:35 -0700

    Create 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints prints all single digit numbers of base 10
+ * starting from 0, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 16; d++)
+		printf("%x", d);
+	printf("\n");
+	return (0);
+}
