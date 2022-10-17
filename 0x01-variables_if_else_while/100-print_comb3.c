9b99a3826ad12a00974b57fb3c6cfedf054121c8
Paul Njuga <paulnjuga@gmail.com>
2022-10-16 20:10:46 -0700

    Create 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two digits
+ * @i - First digit number
+ * @j - Second digit number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			/**
+			 * Both digits must be different i.e. !11, 22... (i != j) solves this
+			 *
+			 * Any value in the second column less than the value in the first column,
+			 * would create a repeat value i.e. 01 = 10 (j > i) solves this
+			 */
+			if (i != j && j > i)
+			{
+				putchar(i);
+				putchar(j);
+				/*Handles punctuations*/
+				if (i != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
