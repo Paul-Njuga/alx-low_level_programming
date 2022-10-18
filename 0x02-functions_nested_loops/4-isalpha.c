95414b932649aa0902ea1b17778dd333d42daa6a
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 00:13:20 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to check
+ *
+ * Return: 1 (Success), 0 (Otherwise)
+ */
+int _isalpha(int c);
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
