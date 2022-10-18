c3d5d73191386867f6f107a750c93bb52f2f1003
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 00:30:19 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: i
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i < 0)
+	{
+		i = i * (-1);
+		return (i);
+	}
+	else
+	{
+		return (i);
+	}
+}
