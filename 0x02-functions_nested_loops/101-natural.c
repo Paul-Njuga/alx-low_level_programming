4f8f63d94cbbee8771d89c2f925687c71c311b74
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 05:56:30 +0300

    Create 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,14 +6,16 @@
  *
  * Return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i, sum;
+
 	for (i = 0; i < 1024; i++)
 	{
 		if (((i % 3) == 0) || ((i % 5) == 0))
 			sum += i;
 	}
 	printf("%d", sum);
+	printf("\n");
 	return (0);
 }
