1dc79e7401653a84890f712bbe896a41763faa3c
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 05:51:04 +0300

    Create 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,10 +3,8 @@
 /**
  * main - computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded),
  * followed by a new line
- *
- * Return: sum
  */
-int main (void)
+void main (void)
 {
 	int i, sum;
 	for (i = 0; i < 1024; i++)
