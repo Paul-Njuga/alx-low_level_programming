0d036908f8efa36205bfef85d14676d3bf064889
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 06:47:14 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,12 +14,12 @@ int main(void)
 	int i, n3, sum;
 	for (i = 3; i <= 4000000; i++)
 	{
-		n3 = n1 + n2
+		n3 = n1 + n2;
 		if (n3 % 2 == 0)
 			sum += n3;
 		printf("%d, "sum);
-		n1 = n2
-		n2 = n3
+		n1 = n2;
+		n2 = n3;
 	}
 	return (0);
 }
