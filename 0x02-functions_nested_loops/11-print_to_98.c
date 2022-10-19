79437a5377f47be8ed54e97be0282b47682e4fec
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 04:45:25 +0300

    Create 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
- * @n - print from this number
+ * @n: print from this number
  */
 void print_to_98(int n)
 {
