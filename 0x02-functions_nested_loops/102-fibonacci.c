9ec26ecef4233f413b3d3d65676276e53da55271
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 07:09:43 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -17,11 +17,11 @@ int main(void)
 
 	for (i = 1; i <= 50; i++)
 	{
-		n3 = n1 + n2;
 		if (i != 50)
 			printf("%ld, ", n3);
 		else
 			printf("%ld\n", n3);
+		n3 = n1 + n2;
 		n1 = n2;
 		n2 = n3;
 	}
