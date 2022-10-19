8a04ec16029d4a56c0821e78a07e792ee3c1301d
Paul Njuga <paulnjuga@gmail.com>
2022-10-19 05:16:44 +0300

    Create 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,9 @@ void print_times_table(int n)
 
 	if (!(n > 15 || n < 0))
 	{
-		for (i = 0; i <= 9; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= 9; j++)
+			for (j = 0; j <= n; j++)
 			{
 				m = i * j;
 				if (m >= 100)
