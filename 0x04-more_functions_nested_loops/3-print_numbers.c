ee5aee632290385dbf8afc1033bd53f7f4c4a1e3
Paul Njuga <paulnjuga@gmail.com>
2022-10-21 23:46:59 +0300

    Add 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - prints the numbers,
+ * print_numbers - prints the numbers,
  * from 0 to 9
  * followed by a new line
  */
-void print_alphabet(void)
+void print_numbers(void)
 {
 	int i;
 
