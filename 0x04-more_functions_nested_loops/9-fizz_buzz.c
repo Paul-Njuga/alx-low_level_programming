5ffe4ef304e072cddb1f12bb6908e1990f20df44
Paul Njuga <paulnjuga@gmail.com>
2022-10-24 00:34:51 +0300

    Add 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,7 +20,7 @@ int main(void)
 		else if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("Fizz");
+			printf("FizzBuzz");
 		else
 			printf("%d", i);
 
