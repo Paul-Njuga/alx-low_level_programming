6487edaaf129d06fb2ee6aca0f02e869048e0f1f
Paul Njuga <paulnjuga@gmail.com>
2022-10-26 23:26:24 +0300

    Create 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,9 +12,9 @@ void puts2(char *str)
 
 	len = 0;
 
-	while (*(str + i) != '\0')
-		i++;
-	len = i;
+	/* Gets length */
+	while (*(str + len) != '\0')
+		len++;
 
 	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
