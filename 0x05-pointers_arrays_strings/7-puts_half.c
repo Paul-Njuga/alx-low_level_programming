14fe679370143c651327b940fdd7631d25eb90c6
Paul Njuga <paulnjuga@gmail.com>
2022-10-27 00:02:33 +0300

    Create 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ void puts_half(char *str)
 
 	/* Get mid of legnth */
 	if (len % 2 != 0)
-		/* Some reverse psychology shit */
+		/* Some reverse psychology stuff */
 		mid = ((len) / 2) + 1;
 	else
 		mid = len / 2;
