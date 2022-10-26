499956ebbe326f147a9716b7b53df5d6dc04b2a8
Paul Njuga <paulnjuga@gmail.com>
2022-10-26 23:42:30 +0300

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string,
+ * followed by a new line
+ * @str: pointer to char(s)
+ */
+void puts_half(char *str)
+{
+	int i, len, mid;
+
+	len = 0;
+
+	/* Gets length */
+	while (*(str + len) != '\0')
+		len++;
+
+	/* Get mid of legnth */
+	if (len % 2 != 0)
+		mid = (len - 1) / 2;
+	else
+		mid = len / 2;
+
+	for (i = mid; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
