8dd04c91c0a3450f708dd5c642ddd0f508bbe127
Paul Njuga <paulnjuga@gmail.com>
2022-10-27 01:45:07 +0300

    Create 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,8 @@
 /**
  * print_array - prints n elements of an array of integers,
  * followed by a new line
- * @a - pointer to array
- * @n - no. of elements of the array to be printed
+ * @a: pointer to array
+ * @n: no. of elements of the array to be printed
  */
 void print_array(int *a, int n)
 {
