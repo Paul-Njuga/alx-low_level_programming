85d7ab7969ffe0de781882a923923ff64602fef4
Paul Njuga <paulnjuga@gmail.com>
2022-10-26 23:57:42 +0300

    Create 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,9 @@ void puts_half(char *str)
 
 	/* Get mid of legnth */
 	if (len % 2 != 0)
-	{
-		mid = (len - 1) / 2;
-		mid--;
-	{
+		mid = ((len - 1) / 2) - 1;
 	else
-	{
 		mid = len / 2;
-	}
 
 	for (i = mid; i < len; i++)
 		_putchar(str[i]);
