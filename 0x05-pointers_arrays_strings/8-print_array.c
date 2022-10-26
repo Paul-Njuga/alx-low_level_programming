6774670030c77ff2aa5a793117fa8b4385c4535a
Paul Njuga <paulnjuga@gmail.com>
2022-10-27 01:51:11 +0300

    Create 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,13 @@
  */
 void print_array(int *a, int n)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
 		if (i < n - 1)
 			printf(", ");
-		else if (i < n)
-			printf("");
 	}
 	printf("\n");
 }
