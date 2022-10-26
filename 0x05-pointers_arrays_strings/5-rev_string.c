671e418743e84df3501d72375752d95490cf0f55
Paul Njuga <paulnjuga@gmail.com>
2022-10-26 06:56:55 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to char(s)
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	/* Initialize index */
+	i = 0;
+	/* Move i to last character */
+	while (*(s + i) != '\0')
+		i++;
+	i--;
+	/* Start moving backwards from current index i */
+	for (; i >= 0; i--)
+		(*(s + i));
+}
