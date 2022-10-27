25c0eff420699c7b7cd2c544aa399a6036df88d0
Paul Njuga <paulnjuga@gmail.com>
2022-10-27 05:15:33 +0300

    Create 9-strcpy.c, 100-atoi.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
