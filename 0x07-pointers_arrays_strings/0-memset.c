659cd3a7073cb526f6953e82fe45acc2c5c7b82f
Paul Njuga <paulnjuga@gmail.com>
2022-11-02 01:25:16 +0300

    Create 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,11 +5,12 @@
  * @s: pointer to char memory to fill
  * @b: constant byte to fill
  * @n: bytes to be filled
+ *
  * Return: pointer to char memory s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		*(s + i) = b;
