ad39296e6ca62a12a7d52dc05ce1f04fd1f3a795
Paul Njuga <paulnjuga@gmail.com>
2022-11-04 05:02:59 +0300

    101 still throwing errors

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1,
+ * if the strings can be considered identical
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 1 if the strings can be considered identical,
+ * otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	/* Will update */
+	return (0);
+}
