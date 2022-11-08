4f371826ed4c6e17054e2b7a21c9ce3fff6ebe06
Paul Njuga <paulnjuga@gmail.com>
2022-11-08 04:32:17 +0300

    Correct 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - prints its name,
  * followed by a new line
  * @argc: argument count
- * @argv: double pointer to char
+ * @argv: string of pointers
  *
  * Return: Always 0 (Success)
  */
@@ -11,7 +11,6 @@ int main(int argc, char *argv[])
 {
 	if (argv[0] == 'mv' && argc == 3)
 		argv[0] = argv[2];
-	else
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
