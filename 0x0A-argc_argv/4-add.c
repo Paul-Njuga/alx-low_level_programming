d80c4bcf25df2be7a169bb2b8b6bb93b56653405
Paul Njuga <paulnjuga@gmail.com>
2022-11-08 05:35:41 +0300

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: pointer to array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	unsigned int n;
+
+	if (argc < 1)
+	{
+		printf("\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		n = atoi(argv[i]);
+
+		if (n)
+		{
+			sum += n;
+			printf("%d\n", sum);
+			return (0);
+		}
+		else
+		{
+			printf("Error\n", argv[i]);
+			return (1);
+		}
+	}
+}
