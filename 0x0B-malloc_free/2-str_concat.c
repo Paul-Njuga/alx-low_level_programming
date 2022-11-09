82a4adfbc51b1af6bea3079696ac6f65e539242d
Paul Njuga <paulnjuga@gmail.com>
2022-11-10 00:25:03 +0300

    Correct 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,16 +35,15 @@ char *str_concat(char *s1, char *s2)
 		sz1 = 0;
 	else if (s2 == NULL)
 		sz2 = 0;
-
 	sum = sz1 + sz2;
-	p = malloc(sizeof(*p) * (sum + 1));
 
+	p = malloc(sizeof(*p) * (sum + 1));
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i <= sz1; i++)
+	for (i = 0; i < sz1; i++)
 		p[i] = s1[i];
-	for (; i <= sum; i++)
+	for (; i < sum; i++)
 		p[i] = s2[i];
 	p[sum + 1] = '\0';
 
