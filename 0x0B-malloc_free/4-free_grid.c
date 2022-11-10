e4f933659b0431734429294a015a4f62305d5a1f
Paul Njuga <paulnjuga@gmail.com>
2022-11-10 03:08:23 +0300

    Create 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - returns pointer to a 2d array of int
+ * @grid: grid to free
+ * @height: grid height
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return (NULL);
+
+	/* Free malloc'd pointers in column */
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	/* Free malloc'd array of pointers in row */
+	free(grid);
+}
