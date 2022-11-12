42dbea76548b065865aa400af23dcd1b09b383b6
Paul Njuga <paulnjuga@gmail.com>
2022-11-13 01:18:13 +0300

    Correct 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
 
 /**
  * main - adds positive numbers
@@ -10,24 +54,31 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-	unsigned int n;
+	int sum, num, i, j, k;
+
+	sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		n = atoi(argv[i]);
-
-		if (n)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum += n;
-			printf("%d\n", sum);
-			return (0);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
 		}
 	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
 	return (0);
 }
