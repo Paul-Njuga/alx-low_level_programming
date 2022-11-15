c54225ecd0cb6bf69ecdcf2571fc34e0cf226a70
Paul Njuga <paulnjuga@gmail.com>
2022-11-16 02:28:32 +0300

    Create typedef dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  * @age: dog age
  * @owner: pointer to owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
