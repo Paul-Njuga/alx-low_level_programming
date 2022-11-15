c711628afffd02215c9255844d2c1965fc9d0952
Paul Njuga <paulnjuga@gmail.com>
2022-11-16 01:06:28 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog attributes
+ * @name: dog name
+ * @age: dog age
+ * @owner: pointer to owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
