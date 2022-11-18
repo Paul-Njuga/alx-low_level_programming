6a3e03a8302473fd135f2cb0f1dd2e326bdc1400
Paul Njuga <paulnjuga@gmail.com>
2022-11-18 19:25:04 +0300

    Correct  print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * print - callback function
- * @name: name to be printed
- */
-void print(char *name)
-{
-	printf("Hello, my name %s", name);
-}
-
-/**
  * print_name - prints a name
  * @name: name to be printed
  * @f: callback function
