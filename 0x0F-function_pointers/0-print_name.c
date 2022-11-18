2bdefe80e8f2ab50832ba43b9b49fe9f82379cee
Paul Njuga <paulnjuga@gmail.com>
2022-11-18 19:11:27 +0300

    Create print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,18 +6,19 @@
  */
 void print(char *name)
 {
-    printf("Hello, my name %s", name);
+	printf("Hello, my name %s", name);
 }
 
 /**
- * print name - prints a name
+ * print_name - prints a name
  * @name: name to be printed
  * @f: callback function
  * @: name to be printed
  */
 void print_name(char *name, void (*f)(char *))
 {
-    void (*f)(char *);
-    f = print;
-    f(name);
+	void (*f)(char *);
+
+	f = print;
+	f(name);
 }
