6d696a6923e64b05dd12c0e56bf5deb3c588eb92
Paul Njuga <paulnjuga@gmail.com>
2022-11-20 19:44:35 +0300

    Func point corrections

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,9 +20,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s[0])
+		/* Using strcmp lets you compare the whole string */
+		if (strcmp(ops[i].op, s == 0))
 			return (ops[i].f);
 		i++;
 	}
