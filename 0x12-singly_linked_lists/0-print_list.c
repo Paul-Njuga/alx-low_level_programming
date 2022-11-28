33484b085eb5af63a857fce92518530be857e002
Paul Njuga <paulnjuga@gmail.com>
2022-11-29 02:34:26 +0300

    Add node count & handle !str

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,22 +3,22 @@
 /**
  * print_list - prints all the elements of a list_t list
  * @h: list_t node to print
- * 
+ *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	if (h->str == NULL)
-		printf("[0] (nil)");
+	unsigned long node = 0; /* Count for node */
 
-	/* struct node saves struct h's address */
-	list_t *node = (list_t *)malloc(sizeof(list_t));
-	node = h;
-
-	while (node != NULL)
-	{	
-		printf("[%u] %s\n", node->len, node->str);
-		node = node->next;
+	while (h)
+	{
+		if (!(h->str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+	
+		h = h->next;
+		node++;
 	}
-	return (node->len);
+	return (node);
 }
