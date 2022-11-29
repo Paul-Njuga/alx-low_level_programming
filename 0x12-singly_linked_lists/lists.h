d3a2b836bbb1f60469f8421494c735153f7ef082
Paul Njuga <paulnjuga@gmail.com>
2022-11-29 03:52:10 +0300

    Picky organizer

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,9 +21,9 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+int _strlen(const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int _strlen(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
