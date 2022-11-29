5b95597a6433afef28befc5f570da927fa94badf
Paul Njuga <paulnjuga@gmail.com>
2022-11-29 03:20:04 +0300

    Add string len handler

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * _strlen - calc length of a string
+ * @str: string to be calc
+ * Return: len of a string
+ */
+
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: double pointer to the first node
  * @str: string to be duplicated
@@ -17,10 +32,8 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(*str);
-	/* If list is empty head = new */
-	if (head == NULL)
-		head = new;
-	new->next = head;
-	head = new;
+	new->len = _strlen(*str);
+	new->next = *head;
+	*head = new;
 	return (head);
 }
