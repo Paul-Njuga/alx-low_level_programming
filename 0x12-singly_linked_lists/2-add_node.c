8e57a2bf20b40b31b83085d0664023ab062cd995
Paul Njuga <paulnjuga@gmail.com>
2022-11-29 03:32:36 +0300

    Correct arg type passed to _strlen()

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -31,9 +31,9 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
-	new->str = strdup(*str);
-	new->len = _strlen(*str);
+	new->str = strdup(str);
+	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
-	return (head);
+	return (*head);
 }
