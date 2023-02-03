712bff5018bf6225a47e205f07f2047f56f149e9
Paul Njuga <paulnjuga@gmail.com>
2023-02-04 01:13:18 +0300

    Add hash table func

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,17 +9,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t **new_array;
+	unsigned long int i;
 
-	if (size == 0)
+	if (size <= 0)
 		return (NULL);
 
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
+
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t *));
-	if (!(table->array))
+
+	new_array = calloc(table->size, sizeof(hash_node_t *));
+	if (new_array == NULL)
 		return (NULL);
+
+	table->array = new_array;
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
