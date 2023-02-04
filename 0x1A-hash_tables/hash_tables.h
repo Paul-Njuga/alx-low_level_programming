f2f7141d268dfae8854f59d43b073bcfb6b9dad6
Paul Njuga <paulnjuga@gmail.com>
2023-02-04 05:55:23 +0300

    Add free hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,7 @@ hash_node_t *create_item(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void free_list(hash_node_t *head);
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
