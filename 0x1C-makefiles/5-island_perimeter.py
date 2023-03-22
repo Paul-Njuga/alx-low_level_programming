98553c41fb6f3021c3ef98f446cb34f65af5f6ad
Paul Njuga <paulnjuga@gmail.com>
2023-03-23 02:21:27 +0300

    Add makefile obj, src & python func

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Island Perimeter: """
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    length = len(grid)
+    size = 0
+    edges = 0
+
+    for i in range(length):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
