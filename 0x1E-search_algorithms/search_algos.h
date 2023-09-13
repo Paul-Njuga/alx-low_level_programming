69d04b0d66aec001b5cbf6097e9d110ad69a89a2
Paul Njuga <paulnjuga@gmail.com>
2023-09-13 18:27:40 +0300

    Add linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SRCH_ALG_H
+#define SRCH_ALG_H
+#include <stdio.h>
+
+/* Function Defs */
+void _putstr(const char *str);
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* SRCH_ALG_H */
