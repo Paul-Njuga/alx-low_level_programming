5a1e1876c25713288d1bc4a7817e0a8571bf5ccf
Paul Njuga <paulnjuga@gmail.com>
2023-09-14 01:39:19 +0300

    Add jump_search func def

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,10 +1,12 @@
 #ifndef SRCH_ALG_H
 #define SRCH_ALG_H
 #include <stdio.h>
+#include<math.h>
 
 /* Function Defs */
 void _putstr(const char *str);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SRCH_ALG_H */
