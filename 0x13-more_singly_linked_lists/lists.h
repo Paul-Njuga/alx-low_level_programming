bb7693624c353ada3cabe7341d73d981ba39a8ca
Paul Njuga <paulnjuga@gmail.com>
2023-11-24 12:00:20 +0300

    Fix: correct prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,19 +1,19 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: singly linked list node structures
+ *
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -37,4 +37,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif /* LISTS_H */
+#endif /*  LISTS_H  */
