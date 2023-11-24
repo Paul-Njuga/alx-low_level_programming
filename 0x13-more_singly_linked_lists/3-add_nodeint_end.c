a085da123dd2d3d147468902091e8da3db3ea078
Paul Njuga <paulnjuga@gmail.com>
2023-11-24 11:41:39 +0300

    Fix: append 2 elements to an empty list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,39 +1,31 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node,
- * at the end of a listint_t list
- * @head: linked list head
- * @n: node data
- *
- * Return: address of the new element (Success),
- * Null (Failure)
+ * add_nodeint_end - add node at the end of the list
+ *@head:pointer to last node
+ *@n:integer elment in node
+ * Return:new node
  */
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new, *start;
 
-	listint_t *last = *head;
-
-	if (!new)
+	start = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = NULL;
-
-	/* If list empty, head = new */
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
+	while (start->next)
+	{
+		start = start->next;
+	}
 
-	/* Traverse till last node */
-	if (last->next != NULL)
-		last = last->next;
-
-	/* Add new to last->next */
-	last->next = new;
+	start->next = new;
 	return (new);
 }
